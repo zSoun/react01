73c0fa6b39df336927f14c293a127f91735c6a4c
Visitante <visitante.digital@digitalcollegesul.local>
2024-04-04 14:14:30 -0300

    first commit

---
+++ dripstore/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Components/Header"
+
+function App() {
+
+
+  return (
+    <>
+        <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ dripstore/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+
+function Header() {
+
+
+    return (
+      <>
+          <h1>Este é um Cabeçalho!</h1>
+      </>
+    )
+  }
+  
+  export default Header
+  
