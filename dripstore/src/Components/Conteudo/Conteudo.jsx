e4cc83faef3962085f860045219715b284e22756
Visitante <visitante.digital@digitalcollegesul.local>
2024-04-04 16:49:59 -0300

    third commit

--- dripstore/src/Components/Conteudo/Conteudo.jsx
+++ dripstore/src/Components/Conteudo/Conteudo.jsx
@@ -1,10 +1,10 @@
 import "./Conteudo.css"
 
 function Conteudo() {
-
-
+  // Aqui fica o JavaScript
     return (
       <>
+          {/* aqui fica o html */}
           <h1 className="Conteudo">Este é o conteúdo do site!</h1>
       </>
     )
--- dripstore/src/Components/Header/Header.jsx
+++ dripstore/src/Components/Header/Header.jsx
@@ -2,19 +2,25 @@ import "./Header.css"
 // import Logo from ""
 
 function Header() {
-
     return (
       <>
-        <div className="Header">
+      <div className="Header">
+        <div className="Cabecalho">
           <h1>DigitalStore</h1>
-          <input type="search" name="search" id="seach" />
-          <div>
+          <input type="search" name="search" id="seach" placeholder="Pesquisar produto..."/>
+          <div className="button">
             <a href="">Cadastre-se</a>
             <button className="entrarBtn">Entrar</button>
           </div>
         </div>
-          
-      </>
+        <div className="Menu">
+          <a href="">Home</a>
+          <a href="">Produtos</a>
+          <a href="">Categorias</a>
+          <a href="">Meus Pedidos</a>
+        </div>
+        </div>
+      </> 
     )
   }
   
